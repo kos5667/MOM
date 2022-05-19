a6abbb24546cc739beace338752a230a4a32d637
kos5667 <kos5667@gmail.com>
2022-05-20 00:01:19 +0900

    Kafka Sample test

---
+++ src/main/java/com/demo/main/kafka/consumer/KafkaSampleConsumerService.java
@@ -0,0 +1,15 @@
+package com.demo.main.kafka.consumer;
+
+import java.io.IOException;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaSampleConsumerService {
+
+	@KafkaListener(topics = "oingdaddy", groupId = "group-id-oing")
+	public void consume(String message) throws IOException {
+		System.out.println("receive message : " + message);
+	}
+}
---
+++ src/main/java/com/demo/main/kafka/producer/KafkaSampleProducerService.java
@@ -0,0 +1,17 @@
+package com.demo.main.kafka.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaSampleProducerService {
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	public void sendMessage(String message) {
+		System.out.println("send message : " + message);
+		this.kafkaTemplate.send("oingdaddy", message);
+	}
+}
