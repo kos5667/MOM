37e8c57cfa1b2e93927b7bf7f3273239ecaf39eb
kos5667 <kos5667@gmail.com>
2022-05-25 22:59:00 +0900

    commit

---
+++ src/main/java/com/demo/main/kafka/vo/CommonVO.java
@@ -0,0 +1,24 @@
+package com.demo.main.kafka.vo;
+
+public class CommonVO {
+
+	private String pageSize;
+	
+	private String index;
+
+	public String getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(String pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+}
